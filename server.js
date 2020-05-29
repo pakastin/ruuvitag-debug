38d84cb7307ae7e8f56298dccbf904586c09c8aa
Juha Lindstedt <juha@pakastin.fi>
2020-05-29 12:20:13 +0300

    Show all parameters

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 const ruuvi = require('node-ruuvitag');
 
 ruuvi.on('found', tag => {
-  let previous;
-
   tag.on('updated', data => {
-    const { id } = tag;
-
-    const deltaX = previous ? (previous.accelerationX - data.accelerationX) : 0;
-    const deltaY = previous ? (previous.accelerationY - data.accelerationY) : 0;
-    const deltaZ = previous ? (previous.accelerationZ - data.accelerationZ) : 0;
-
-    const movement = previous ? (Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2) + Math.pow(deltaZ, 2)) / 1000) : 0;
-
-    previous = data;
+    const { id, address, addressType, connectable } = tag;
 
     console.log({
       id,
-      movement,
+      address,
+      addressType,
+      connectable,
       ...data
     });
   });
